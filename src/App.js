8ffe2500a698ed6bd39918dfbb4b6f7bdf4efa67
viet.dinhduc <ducviet2504@gmail.com>
2021-03-08 23:50:00 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import TopMenu from "./components/TopMenu";
-import Products from "./pages/Products";
-import SignIn from "./Auth/pages/SignIn/SignIn";
-import React, { useEffect, useState } from "react";
-import firebase from "firebase";
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TopMenu from './components/TopMenu';
+import Products from './pages/Products';
+import SignIn from './Auth/pages/SignIn/SignIn';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase';
 
 // Configure Firebase.
 const config = {
-  apiKey: "AIzaSyCbcZMUj2r-ufphdDP8_2U69NMw4U3plO8",
-  authDomain: "auth-app-97c48.firebaseapp.com",
+  apiKey: 'AIzaSyCbcZMUj2r-ufphdDP8_2U69NMw4U3plO8',
+  authDomain: 'auth-app-97c48.firebaseapp.com',
 };
 
 const Index = () => <h2>Home</h2>;
@@ -41,21 +41,19 @@ function App() {
   // }, []);
 
   useEffect(() => {
-    const unregisterAuthObserver = firebase
-      .auth()
-      .onAuthStateChanged(async (user) => {
-        if (!user) {
-          //user logout , handle something here
-          console.log("user is not login");
-          return;
-        }
-        console.log("Loged in user: ", user.displayName);
-        const token = await user.getIdToken();
-        console.log("Logged in user token: ", token);
-        console.log(user);
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
+      if (!user) {
+        //user logout , handle something here
+        console.log('user is not login');
+        return;
+      }
+      console.log('Loged in user: ', user.displayName);
+      const token = await user.getIdToken();
+      console.log('Logged in user token: ', token);
+      console.log(user);
 
-        setIsSignedIn(!!user);
-      });
+      setIsSignedIn(!!user);
+    });
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
 
